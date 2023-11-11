4d2f2cdfef9e5e8b8e1177c4e1b7bff6bd001940
willclem100 <will.clem@biteninja.com>
2023-11-11 07:01:12 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+print("What is the best football team?")
+u = input()
+if u.upper() == "ALABAMA" or u.upper() == "MIAMI DOLPHINS" or u.upper() == "BAMA" or u.upper() == "TENNESSEE":
+    print(f"Fantastico, {u} was the correct answer!")
+    
+else:
+    print("Incorrecto")
